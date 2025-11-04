c19006e333fd4f9346b2906135f55d251ded3cdb
Sebastian <sebastianbeltran341@gmail.com>
2025-11-04 14:53:19 -0500

    pixel

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1322800222724961');
+          fbq('init', '1591221842320517');
           fbq('track', 'PageView');
         `,
           }}
@@ -61,7 +61,7 @@ export default function RootLayout({ children }) {
               height="1"
               width="1"
               style={{ display: "none" }}
-              src="https://www.facebook.com/tr?id=1322800222724961&ev=PageView&noscript=1"
+              src="https://www.facebook.com/tr?id=1591221842320517&ev=PageView&noscript=1"
             />
           </noscript>
         </body>
